d1d4a8223ad506200cbf45c453d3304656f109a9
maaaaz <maaaaz@androwarn.com>
2012-06-18 17:52:22 -0400

    License information added

--- androwarn/report/report.py
+++ androwarn/report/report.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# This file is part of Androwarn.
+#
+# Copyright (C) 2012, Thomas Debize <tdebize at mail.com>
+# All rights reserved.
+#
+# Androwarn is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Androwarn is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Androwarn.  If not, see <http://www.gnu.org/licenses/>.
+
 # Global imports
 import logging
 from jinja2 import Environment, PackageLoader, FileSystemLoader, Template
