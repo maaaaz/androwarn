3e5fd5db4ef43da619dbaa26c3561245f926e0aa
maaaaz <maaaaz@androwarn.com>
2012-07-01 09:30:40 -0400

    fixed import

--- androwarn/report/report.py
+++ androwarn/report/report.py
@@ -29,7 +29,7 @@ log = logging.getLogger('log')
 try :
 	from jinja2 import Environment, PackageLoader, FileSystemLoader, Template
 except ImportError :
-	log.error("The Jinja2 module is not installed, please install it and try again")
+	print "The Jinja2 module is not installed, please install it and try again"
 
 # Androwarn modules import
 from androwarn.search.search import *
